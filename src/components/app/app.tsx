f20104e982747f6bd8d1bd6c83f234674aa94b13
Alexander Filipyev <MrLargusFrey@yandex.ru>
2026-08-16 15:08:14 +0500

    готовый проект

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,55 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './../../constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './../../constants/articleProps';
+import { ArrowButton } from 'src/ui/arrow-button';
 
 import styles from './app.module.scss';
 
 export const App = () => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const [currentState, setCurrentState] =
+		useState<ArticleStateType>(defaultArticleState);
+
+	const handleApply = (newState: ArticleStateType) => {
+		setCurrentState(newState);
+		setIsSidebarOpen(false);
+	};
+
+	const handleReset = () => {
+		setCurrentState(defaultArticleState);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': currentState.fontFamilyOption.value,
+					'--font-size': currentState.fontSizeOption.value,
+					'--font-color': currentState.fontColor.value,
+					'--container-width': currentState.contentWidth.value,
+					'--bg-color': currentState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<div className={styles.arrowButton}>
+				<ArrowButton
+					isOpen={isSidebarOpen}
+					onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+				/>
+			</div>
+			<ArticleParamsForm
+				isOpen={isSidebarOpen}
+				onClose={() => setIsSidebarOpen(false)}
+				onApply={handleApply}
+				onReset={handleReset}
+				currentState={currentState}
+			/>
 			<Article />
 		</main>
 	);
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,16 +1,142 @@
-import { ArrowButton } from 'src/ui/arrow-button';
+import { useState, useEffect } from 'react';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
+
+import {
+	ArticleStateType,
+	defaultArticleState,
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+} from '../../constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+interface ArticleParamsFormProps {
+	isOpen: boolean;
+	onClose: () => void;
+	onApply: (state: ArticleStateType) => void;
+	onReset: () => void;
+	currentState: ArticleStateType;
+}
+
+export const ArticleParamsForm = ({
+	isOpen,
+	onClose,
+	onApply,
+	onReset,
+	currentState,
+}: ArticleParamsFormProps) => {
+	const [formState, setFormState] = useState<ArticleStateType>(currentState);
+
+	useEffect(() => {
+		if (isOpen) {
+			setFormState(currentState);
+		}
+	}, [isOpen, currentState]);
+
+	const handleFontFamilyChange = (value: (typeof fontFamilyOptions)[0]) => {
+		setFormState({ ...formState, fontFamilyOption: value });
+	};
+
+	const handleFontSizeChange = (value: (typeof fontSizeOptions)[0]) => {
+		setFormState({ ...formState, fontSizeOption: value });
+	};
+
+	const handleFontColorChange = (value: (typeof fontColors)[0]) => {
+		setFormState({ ...formState, fontColor: value });
+	};
+
+	const handleBackgroundColorChange = (value: (typeof backgroundColors)[0]) => {
+		setFormState({ ...formState, backgroundColor: value });
+	};
+
+	const handleContentWidthChange = (value: (typeof contentWidthArr)[0]) => {
+		setFormState({ ...formState, contentWidth: value });
+	};
+
+	const handleSubmit = (event: React.FormEvent) => {
+		event.preventDefault();
+		onApply(formState);
+	};
+
+	const handleReset = () => {
+		setFormState(defaultArticleState);
+		onReset();
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<div
+				className={styles.overlay}
+				style={{ display: isOpen ? 'block' : 'none' }}
+				onClick={onClose}
+			/>
+			<aside
+				className={`${styles.container} ${
+					isOpen ? styles.container_open : ''
+				}`}>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<Text as='h2' size={31} weight={800} uppercase>
+						Настройки
+					</Text>
+
+					<Select
+						options={fontFamilyOptions}
+						selected={formState.fontFamilyOption}
+						onChange={handleFontFamilyChange}
+						title='Шрифт'
+					/>
+
+					<Separator />
+
+					<RadioGroup
+						name='fontSize'
+						options={fontSizeOptions}
+						selected={formState.fontSizeOption}
+						onChange={handleFontSizeChange}
+						title='Размер шрифта'
+					/>
+
+					<Separator />
+
+					<Select
+						options={fontColors}
+						selected={formState.fontColor}
+						onChange={handleFontColorChange}
+						title='Цвет шрифта'
+					/>
+
+					<Separator />
+
+					<Select
+						options={backgroundColors}
+						selected={formState.backgroundColor}
+						onChange={handleBackgroundColorChange}
+						title='Цвет фона'
+					/>
+
+					<Separator />
+
+					<Select
+						options={contentWidthArr}
+						selected={formState.contentWidth}
+						onChange={handleContentWidthChange}
+						title='Ширина контента'
+					/>
+
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
